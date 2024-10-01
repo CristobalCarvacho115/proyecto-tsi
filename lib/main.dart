b3b86c2390c5dac09ab714b51a46d8185c2491fb
CristobalCarvacho115 <cris.carvacho.ponce@gmail.com>
2024-10-01 08:53:50 -0300

    listado

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// import 'package:agenda_tsi/pages/home_page.dart';
+import 'package:agenda_tsi/pages/login_page.dart';
+// import 'package:agenda_tsi/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Agenda de actividades',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
